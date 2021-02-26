0b0ea79b74779963c3d30f0419d33c23dbb2a3da
John Doe <mattia.tummolo@libero.it>
2021-02-26 18:51:03 +0100

    push3

--- project/app/Http/Controllers/DishController.php
+++ project/app/Http/Controllers/DishController.php
@@ -15,7 +15,6 @@ class DishController extends Controller
     public function __construct() {
 
         $this->middleware('auth');
-
     }
     
     public function index() {
--- project/app/Http/Controllers/OrderController.php
+++ project/app/Http/Controllers/OrderController.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Payment;
 
 class OrderController extends Controller
 {
+    public function __construct() {
+
+        $this->middleware('auth');
+    }
+    
     public function stats() {
         $orders = Payment::all();
         return view('pages.order-stats', compact('orders'));
